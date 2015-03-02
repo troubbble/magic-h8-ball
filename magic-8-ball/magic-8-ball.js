7ec1c1d8e9b02984944b364929a50699cb5eddc6
troubbble <anne.tipathy@gmail.com>
2015-03-01 21:34:34 -0500

    Made some basic changes like colors and fonts, now just pushing the update to test renaming.

--- magic-8-ball/magic-8-ball.js
+++ magic-8-ball/magic-8-ball.js
@@ -10,6 +10,11 @@ function firstFade() {
 		document.getElementById('btnShake').style.visibility="visible";
 	});
 }
+//Sarah - copied part of the firstFade function so the button would show up//
+
+function btnAppear() {
+	document.getElementById('btnShake').style.visibility="visible";
+}
 
 function numberGenerator() {
 	var number = Math.random();
@@ -17,35 +22,35 @@ function numberGenerator() {
 
 	// 1
 	if (number < 0.125) {
-		document.getElementById('coreText').innerHTML="<br/>Yes.";
+		document.getElementById('coreText').innerHTML="<br/>How the fuck should I know?";
 	}
 	// 2
 	else if (number < 0.25) {
-		document.getElementById('coreText').innerHTML="<br/>It is likely.";
+		document.getElementById('coreText').innerHTML="<br/>Do I look like I give a shit?";
 	}
 	// 3
 	else if (number < 0.375) {
-		document.getElementById('coreText').innerHTML="<br/>Sure, why not? ";
+		document.getElementById('coreText').innerHTML="<br/>You fucking bet. ";
 	}
 	// 4
 	else if (number < 0.5) {
-		document.getElementById('coreText').innerHTML="<br/>No.";
+		document.getElementById('coreText').innerHTML="<br/>No chance in hell.";
 	}
 	// 5
 	else if (number < 0.625) {
-		document.getElementById('coreText').innerHTML="<br/>I doubt it.";
+		document.getElementById('coreText').innerHTML="<br/>If you were on your death bed, maybe.";
 	}
 	// 6
 	else if (number < 0.75) {
-		document.getElementById('coreText').innerHTML="<br/>Ask Mr. Owl.";
+		document.getElementById('coreText').innerHTML="<br/>Well that's the dumbest question I've heard all day. Congrats, asshole.";
 	}
 	// 7
 	else if (number < 0.875) {
-		document.getElementById('coreText').innerHTML="I do not know, because I am a ball.";
+		document.getElementById('coreText').innerHTML="Not fucking likely.";
 	}
 	// 8
 	else {
-		document.getElementById('coreText').innerHTML="What? Sorry, I wasn't listening.";
+		document.getElementById('coreText').innerHTML="Come again? I wasn't listening.";
 	}
 }
 
